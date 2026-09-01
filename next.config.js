3f5bba6146dcc0368b85e174a4a4c7e1d2823e0a
apple <apple@apples-MacBook-Pro.local>
2026-09-01 13:21:30 +0530

    Configure no-store Cache-Control headers to purge Vercel Edge CDN caches

--- next.config.js
+++ next.config.js
@@ -9,6 +9,19 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
+          },
+        ],
+      },
+    ];
+  },
   async redirects() {
     return [
       {
